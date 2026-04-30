68f2d37a8bcf0d9ad9686f46589b2a24d810a86f
Hasan <hasankadado@gmail.com>
2026-04-30 13:58:07 -0500

    feat: add EKS cluster and managed node group

---
+++ infra/modules/eks/main.tf
@@ -0,0 +1,36 @@
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+  version  = "1.29"
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+
+  tags = {
+    Name = "${var.project_name}-node-group"
+  }
+}
