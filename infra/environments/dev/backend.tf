1c20897669b6403b5359d170d6cb5d9d0afe7a97
Hasan <hasankadado@gmail.com>
2026-05-06 15:44:08 -0500

    ci: add remote Terraform state and manual apply workflow

--- infra/environments/dev/backend.tf
+++ infra/environments/dev/backend.tf
@@ -1,1 +1,9 @@
-# S3 remote backend will be configured after the initial local Terraform setup.
+terraform {
+  backend "s3" {
+    bucket       = "eks-devops-platform-tfstate-820242908024"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
